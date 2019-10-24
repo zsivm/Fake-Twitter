0f2856726ccad82e8167e5ee6d6d5f4752944c92
zsivm <zsivics.mihael@gmail.com>
2019-10-24 22:08:51 +0200

    refactored code.js, created bunch of useful methods textarea is cleared after post post consists of message and timestamp now

--- code.js
+++ code.js
@@ -1,14 +1,40 @@
-function addParagraphWithMessage() {
-    let container = _getElement("paragraphs");
-    let paragraph = _createElement("P");
-    let message = _getElement("message").value;
+function addTweet() {
+    let oTweetContainer = _getElement("paragraphs");
+    let oTextP = createParagraph(getTextFromInput());
+    let oTimeP = createParagraph(_getTimestamp());
     
-    if(message) {
-        paragraph.appendChild(_createText(message));
-        container.appendChild(paragraph);
+    if(getTextFromInput()) {
+        let oMessageD = createMessageDiv(oTextP, oTimeP);
+        oMessageD.setAttribute("class", "tweet rounded");
+        oTweetContainer.appendChild(oMessageD);
+        _getElement("message").value = "";
     }
 }
 
+function createMessageDiv(oTextParagraph, oTimestampParagraph) {
+    let oDiv = _createElement("DIV");
+    oDiv.appendChild(oTextParagraph);
+    oDiv.appendChild(oTimestampParagraph);
+    return oDiv;
+}
+
+function createParagraph(sText) {
+    let oParagraph = _createElement("P");
+    oParagraph.appendChild(_createText(sText));
+    return oParagraph;
+}
+
+function getTextFromInput() {
+    return _getElement("message").value;
+}
+
+function _getTimestamp() {
+    let oDate = new Date();
+    let options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return oDate.toLocaleDateString("hu-HU", options);
+}
+
+// Utility functions
 function _getElement(sId) {
     return document.getElementById(sId);
 }
